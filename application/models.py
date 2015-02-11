b30ca4c4c6e23263b2fea0d47f3dd8269d116f4d
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-02-11 11:43:07 +0000

    started working on combined annotation app, using flask-skel template

--- application/models.py
+++ application/models.py
@@ -5,19 +5,19 @@ from application import db
 # use a model per blueprint.
 
 ##
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True)
-#     email = db.Column(db.String(80), unique=True)
-#     password = db.Column(db.String(80))
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
 
-#     def __init__(self, username, email, password):
-#         self.username = username
-#         self.email = email
-#         self.password = password
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
-#     def __repr__(self):
-#         return '<User %r>' % (self.username)
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 ##
 # class Log(db.Model):
