77326b60c40acc589b915b70dc90569b32af03a5
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-02-13 10:37:54 +0000

    added user models

--- application/models.py
+++ application/models.py
@@ -1,23 +1,53 @@
 from application import db
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+
+
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(32))
+    current_login_ip = db.Column(db.String(32))
+    login_count = db.Column(db.Integer)
+
+
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
 
 ##
 # Create your own models here and they will be imported automaticaly. or
 # use a model per blueprint.
 
 ##
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(150), unique=True)
+#     email = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+#     def __init__(self, username, email, password):
+#         self.username = username
+#         self.email = email
+#         self.password = password
 
-    def __repr__(self):
-        return '<User %r>' % (self.username)
+#     def __repr__(self):
+#         return '<User %r>' % (self.username)
 
 ##
 # class Log(db.Model):
