f7fac95110d19f057bab0c6258708eceecf11e54
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-02-13 10:39:28 +0000

    updated app settings with locations for task specifications, user-task mappings, data sink locations etc

--- application/default_settings.py
+++ application/default_settings.py
@@ -7,3 +7,18 @@ DEBUG = True
 RELOAD = True
 SECRET_KEY = 'mysecretkeyvalue'
 SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(_basedir, 'db/app_dev.db')
+
+SECURITY_CONFIRMABLE = False
+SECURITY_TRACKABLE = True
+SECURITY_POST_LOGIN_VIEW = '/home'
+SECURITY_RECOVERABLE = True
+SECURITY_CHANGEABLE = True
+
+TASK_DESCS_URI = os.path.join(_basedir, 'tasks/descriptions')
+
+USERMAPS = os.path.join(_basedir, 'tasks/usermaps')
+
+DATA_SOURCE_URIS = os.path.join(_basedir, 'tasks/data_source_uris.json')
+
+DATA_SINK = os.path.join(_basedir, 'data')
+
--- application/production.py
+++ application/production.py
@@ -5,3 +5,7 @@ RELOAD = False
 CSRF_ENABLED = True
 SECRET_KEY = 'notmysecretkey'
 SQLALCHEMY_DATABASE_URI = str(os.environ.get('DATABASE_URL', 'postgresql://localhost/myproddatabase'))
+
+
+#SECURITY_CONFIRMABLE = False
+#SECURITY_TRACKABLE = True
