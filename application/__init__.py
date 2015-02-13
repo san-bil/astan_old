5563eba45a51a42f8d9188004a513004d36503ab
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-02-13 10:40:18 +0000

    added code to package init that reads in all the task defs, user-task mappings, necessary locations etc

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore
+import os  
+import json
 
 # Create the app and configuration
 # Read the configuration file
@@ -11,19 +14,57 @@ app.config.from_envvar('PRODUCTION_SETTINGS', silent=True)
 db = SQLAlchemy(app)
 
 
-# 404 page not found "route"
-@app.errorhandler(404)
-def not_found(error):
-    title = "404 Page not found"
-    return render_template('404.html', title=title), 404
+from application.models import User, Role
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 
-# 500 server error "route"
-@app.errorhandler(500)
-def server_error(error):
-    title = "500 Server Error"
-    db.session.rollback()
-    return render_template('500.html', title=title), 500
 
+# Create a user to test with
+@app.before_first_request
+def create_user():
+	db_uri = app.config['SQLALCHEMY_DATABASE_URI']
+	db_file_uri = db_uri.split("sqlite:////")[1]
+	if not os.path.isfile(db_file_uri):
+		print "making new db"
+		db.create_all()
+		user_datastore.create_user(email='sanjay@bil.com', password='password')
+		db.session.commit()
+
+
+def get_task_defs():
+	task_defs_obj = read_all_json_configs(app.config['TASK_DESCS_URI'])
+	task_defs_list = task_defs_obj[0]["task_defs"]
+	data_uris = get_data_uris()
+	updated_task_defs_list = join_tasks_with_uris(task_defs_list, data_uris)
+	return updated_task_defs_list
+	
+def get_usermaps():
+	tmp = read_all_json_configs(app.config['USERMAPS'])
+	return tmp[0]["users"]
+
+def get_data_uris():
+	with open (app.config['DATA_SOURCE_URIS']) as tmpfile:
+		tmpdata=json.loads(tmpfile.read())
+		return tmpdata
+
+def join_tasks_with_uris(task_defs, data_uris):
+    updated_task_defs = []
+    for task in task_defs:
+        task.update(data_uri=data_uris[task["data_source_name"]])
+        updated_task_defs.append(task)
+    return updated_task_defs
+
+def read_all_json_configs(tmppath):
+	all_configs = []
+	for root, directories, filenames in os.walk(tmppath):
+		for filename in filenames: 
+			with open (os.path.join(root,filename)) as tmpfile:
+				tmpdata=json.loads(tmpfile.read())
+				all_configs.append(tmpdata)
+	return all_configs
+
+task_defs = get_task_defs()
+usermaps = get_usermaps()
 
 import application.manager
