662344d6664d4dcb5b4913531f6ebaa00a55d6ef
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-06-23 11:41:14 +0100

    removed debug

--- runserver.py
+++ runserver.py
@@ -1,8 +1,16 @@
 import os
 from application import app
+import logging
 
 # run the application!
 if __name__ == '__main__':
      # Bind to PORT if defined, otherwise default to 5000.
+ 
+    
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+ 
+ 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
